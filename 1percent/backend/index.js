47a229dc09632d1c69d84c6e689f5119b165dbe3
AIME-LOIC <kingaime132@gmail.com>
2026-09-08 16:47:05 +0200

    Revert "feat: add /api/challenges route with list and submit endpoints"
    This reverts commit 02247d80f18311e0914bac7f7cb5f541d4d0decd.

--- 1percent/backend/index.js
+++ 1percent/backend/index.js
@@ -23,10 +23,8 @@ const pdfRoutes = require('./routes/pdfRoutes');
 const premiumRoutes = require('./routes/premiumRoutes');
 const coinsRoutes = require('./routes/coinsRoutes');
 const labRoutes = require('./routes/labRoutes');
-const docsRoutes = require('./routes/docsRoutes');
 const signatureRoutes = require('./routes/signatureRoutes');
 const fileRoutes = require('./routes/fileRoutes');
-const challengesRoutes = require('./routes/challengesRoutes');
 
 const app = express();
 
@@ -126,8 +124,6 @@ app.use('/api/coins', coinsRoutes);
 app.use('/api/lab', labRoutes);
 app.use('/api/sign', signatureRoutes);
 app.use('/api/files', fileRoutes);
-app.use('/api/challenges', challengesRoutes);
-app.use('/api/docs', docsRoutes);
 app.use('/api', courseRoutes);  // /api/roadmap, /api/courses (has /:slug)
 
 /* ============================================================
@@ -146,7 +142,6 @@ const htmlRoutes = {
   '/learn/certificate': 'certificate-view.html',
   '/admin': 'admin.html',
   '/admin/': 'admin.html',
-  '/docs': 'docs.html',
 };
 
 app.get('*', (req, res) => {
