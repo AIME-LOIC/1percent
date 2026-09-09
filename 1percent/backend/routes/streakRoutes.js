478decd8a100d019b4cae908ad316133d7937e84
AIME-LOIC <kingaime132@gmail.com>
2026-09-09 13:36:37 +0200

    feat: coin-locked lessons, leaderboard panel, streak fix, -3 coin penalty for missed days

--- 1percent/backend/routes/streakRoutes.js
+++ 1percent/backend/routes/streakRoutes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const streakController = require('../controllers/streakController');
-const { authenticate } = require('../middlewares/auth');
+const { authenticate, optionalAuth } = require('../middlewares/auth');
 
 const router = Router();
 
 router.get('/', authenticate, (req, res, next) => streakController.getStreak(req, res, next));
+router.get('/leaderboard', optionalAuth, (req, res, next) => streakController.getLeaderboard(req, res, next));
+router.post('/daily-penalty', (req, res, next) => streakController.runDailyPenalty(req, res, next));
 
 module.exports = router;
